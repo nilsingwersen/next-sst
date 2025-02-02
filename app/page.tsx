ce724cd13d737dba7b0e154939007e460095301d
Nils Ingwersen <ingwersen@ubilabs.com>
2025-02-02 16:58:43 +0100

    chore: updated content

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,9 @@ export default function Home() {
           priority
         />
 
-        <div className="mb-2">Hey this is my Next.js app with SST</div>
+        <div className="mb-2">
+          Hey this is my Next.js app with SST and updated content
+        </div>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
